1ccbe2c7353bf9d2dc13c71d6529c45b8e212f5c
William Bell <willbee71@googlemail.com>
2019-09-16 21:13:01 +0100

    Feature: express request middleware

---
+++ api-ts/src/api/express/middleware/express-request-middleware.test.ts
@@ -0,0 +1,103 @@
+import { Request, Response } from 'express';
+
+import { ILogLine } from '../../../services/logger/ilog-line';
+import { ILogger } from '../../../services/logger/ilogger';
+import { Logger } from '../../../services/logger/logger';
+import { ExpressRequestMiddleware } from './express-request-middleware';
+
+let logLineSpy: jest.Mock;
+let warnLineSpy: jest.Mock;
+let errorLineSpy: jest.Mock;
+let assertLineSpy: jest.Mock;
+let log: ILogLine;
+let warn: ILogLine;
+let error: ILogLine;
+let assert: ILogLine;
+let logger: ILogger;
+let req: Request;
+let res: Response;
+beforeEach(() => {
+  logLineSpy = jest.fn().mockImplementation(() => {});
+  warnLineSpy = jest.fn().mockImplementation(() => {});
+  errorLineSpy = jest.fn().mockImplementation(() => {});
+  assertLineSpy = jest.fn().mockImplementation(() => {});
+
+  log = {log: logLineSpy};
+  warn = {log: warnLineSpy};
+  error = {log: errorLineSpy};
+  assert = {log: assertLineSpy};
+  logger = new Logger(log, warn, error, assert);
+
+  req = {
+    body: {
+      firstName: 'First',
+      lastName: 'Last'
+    }
+  } as unknown as Request;
+  res = {
+    sendStatus: jest.fn()
+  } as unknown as Response;
+});
+afterEach(() => {
+  jest.restoreAllMocks();
+
+  req = {
+    body: {
+      firstName: 'First',
+      lastName: 'Last'
+    }
+  } as unknown as Request;
+  res = {
+    sendStatus: jest.fn()
+  } as unknown as Response;
+});
+
+describe('ExpressRequestMiddleware', () => {
+  describe('validateRequestBodyFields', () => {
+    it('should call logger debug on start', () => {
+      ExpressRequestMiddleware.validateRequestBodyFields(logger, req, res, jest.fn(), ['firstName', 'lastName']);
+
+      expect(logLineSpy).toHaveBeenCalledTimes(2);
+    });
+
+    it('should validate case insenitive property names', () => {
+      const next: jest.Mock = jest.fn();
+      ExpressRequestMiddleware.validateRequestBodyFields(logger, req, res, next, ['FirstName', 'LastName']);
+
+      expect(next).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call logger debug if validation fails', () => {
+      ExpressRequestMiddleware.validateRequestBodyFields(logger, req, res, jest.fn(), ['firstName']);
+
+      expect(logLineSpy).toHaveBeenCalledTimes(2);
+    });
+
+    it('should call res sendStatus 400 if validation fails', () => {
+      ExpressRequestMiddleware.validateRequestBodyFields(logger, req, res, jest.fn(), ['firstName']);
+
+      expect(res.sendStatus).toHaveBeenCalledTimes(1);
+      expect(res.sendStatus).toHaveBeenCalledWith(400);
+    });
+
+    it('should call logger debug if validation passes', () => {
+      ExpressRequestMiddleware.validateRequestBodyFields(logger, req, res, jest.fn(), ['firstName', 'lastName']);
+
+      expect(logLineSpy).toHaveBeenCalledTimes(2);
+    });
+
+    it('should call next if validation passes', () => {
+      const next: jest.Mock = jest.fn();
+      ExpressRequestMiddleware.validateRequestBodyFields(logger, req, res, next, ['firstName', 'lastName']);
+
+      expect(next).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call res send status 400 if it throws an error', () => {
+      ExpressRequestMiddleware.validateRequestBodyFields(logger, req, res, jest.fn(), undefined as unknown as Array<string>);
+
+      expect(res.sendStatus).toHaveBeenCalledTimes(1);
+      expect(res.sendStatus).toHaveBeenCalledWith(400);
+    });
+  });
+});
---
+++ api-ts/src/api/express/middleware/express-request-middleware.ts
@@ -0,0 +1,44 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { ILogger } from '../../../services/logger/ilogger';
+
+/**
+ * Express middleware for validating request body properties against a whitelist.
+ */
+export class ExpressRequestMiddleware {
+  /**
+   * Validate a request body properties against a whitelist.
+   * @param {ILogger} logger - logger service.
+   * @param {Request} req - express request object.
+   * @param {Response} res - express response object.
+   * @param {NextFunction} next - next function in middleware chain.
+   * @param {string[]} validFields - list of fields to whitelist.
+   */
+  public static validateRequestBodyFields(logger: ILogger,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+    validFields: string[]): void {
+    try {
+      logger.debug('ExpressRequestValidators', 'Validating request body properties');
+      // ensure all fields are lowercase
+      validFields = validFields.map((field: string) => field.toLowerCase());
+      // check each body property against field list
+      const bodyProperties: string[] = Object.keys(req.body);
+      for (let propCount: number = 0; propCount < bodyProperties.length; propCount++) {
+        if (-1 === validFields.indexOf(bodyProperties[propCount].toLowerCase())) {
+          // unknown field, bad request
+          logger.debug('ExpressRequestValidators', 'Bad property found in request');
+          res.sendStatus(400);
+          return;
+        }
+      }
+
+      logger.debug('ExpressRequestValidators', 'Property validation passed');
+
+      next();
+    } catch (_) {
+      res.sendStatus(400);
+    }
+  }
+}
